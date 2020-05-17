f8db086dd5e8acce1e498c2260f6251c305453fa
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-05-17 14:20:32 -0300

    Fixing imports.

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import appoimentsRouter from './appointments.routes';
-import usersRoute from './users.routes';
-import sessionRoute from './session.routes';
+import appoimentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRoute from '@modules/users/infra/http/routes/users.routes';
+import sessionRoute from '@modules/users/infra/http/routes/session.routes';
 
 const routes = Router();
 routes.use('/appoiments', appoimentsRouter);
