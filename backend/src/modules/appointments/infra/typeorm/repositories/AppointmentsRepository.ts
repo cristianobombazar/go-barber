7faf9d2c1d0379f3e6b5a7148815c626b993325c
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-07-19 13:42:42 -0300

    Using eager loaging to get user's information.

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -61,6 +61,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${criteria.year}'`
         ),
       },
+      relations: ['user'],
     });
     return findAppointment;
   }
